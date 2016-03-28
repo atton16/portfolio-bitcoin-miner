e9f795624716f3c10610b32be7a6715264449f55
Attawit Kittikrairit <atton16@gmail.com>
2016-03-29 00:25:16 +1100

    unignore *.d files

---
+++ sdk.workspace/SHA256_SW/Debug/src/sha256.d
@@ -0,0 +1,5 @@
+src/sha256.o src/sha256.d: \
+ Y:/sha256.hw.accelerator/sdk.shared_src/sha256.c \
+ Y:/sha256.hw.accelerator/sdk.shared_src/sha256.h
+
+Y:/sha256.hw.accelerator/sdk.shared_src/sha256.h:
