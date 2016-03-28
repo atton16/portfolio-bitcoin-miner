8306d220afdd5972f969049d0db7456dcdbe7e3f
Attawit Kittikrairit <atton16@gmail.com>
2016-03-28 22:18:20 +1100

    Ignore *.d files and delete them all. I think this file should inly be on local system

--- sdk.workspace/SHA256_SW_Profiling/Debug/src/sha256.d
+++
@@ -1,5 +0,0 @@
-src/sha256.o src/sha256.d: \
- Y:/sha256.hw.accelerator/sdk.shared_src/sha256.c \
- Y:/sha256.hw.accelerator/sdk.shared_src/sha256.h
-
-Y:/sha256.hw.accelerator/sdk.shared_src/sha256.h:
